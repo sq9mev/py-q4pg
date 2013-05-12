a566447f9fd2e3a2fed4ae5b0ab04c787ac248e1
Bartek Radwan <bartosz.radwan@proidea.org.pl>
2013-05-12 15:34:28 +0200

    make examples a little more verbose

--- example_consumer.py
+++ example_consumer.py
@@ -1,6 +1,7 @@
+import logging
+from psycopg2 import ProgrammingError
 import taskregistry
 from example_tasks import tq, add, sub
-import logging
 
 
 
@@ -11,4 +12,11 @@ if __name__ == "__main__":
 
     for (k, v) in taskregistry.tasks.get_all().items():
         print '%s: %s' % (k, v)
+
+    try:
+        tq.message_queue.create_table()
+        logging.info('Table has been created!')
+    except ProgrammingError, ex:
+        pass
+
     tq.run()
--- example_producer.py
+++ example_producer.py
@@ -1,8 +1,22 @@
 from time import  sleep
+from psycopg2 import ProgrammingError
+import logging
+import sys
+
 from example_tasks import add, sub
 
+if __name__ == "__main__":
+    logger=logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
-while True:
-    print 'id: %s' % add.delay(1,1)
-    print 'id: %s' % sub.delay(1,1)
-    sleep(1)
+    while True:
+        try:
+            print 'id: %s' % add.delay(1,1)
+            print 'id: %s' % sub.delay(1,1)
+            sleep(1)
+        except ProgrammingError, ex:
+            logging.error(ex)
+            sys.exit('Run example_consumer first, it will create queue table if needed!')
+        except KeyboardInterrupt:
+            sys.exit('No more tasks is beeing produced!')
+
